5d5f6cf649ebd12f1955b0c1109bc413578d947e
Arnon10 <68929256+Arnon10@users.noreply.github.com>
2020-07-29 23:08:21 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function yes(){
+    alert('you found the invisible button!')
+    alert('maybe someone told you about him')
+    alert('maybe it was an accident')
+    alert('either way its impressive!')
+    alert('i need to reward you')
+    alert('it dosent happen every day')
+    alert('press OK to claim your reward')
+    window.location.href='https://youtu.be/gK5IKEgt7e4'
+}
+
+
+
+
+
